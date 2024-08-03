7f13594d4ab7358706deb7cc2d68c0437bc6d989
jose13162 <anselmojose297@gmail.com>
2024-08-03 00:21:30 -0300

    Replace basic html with an actual typescript api

---
+++ src/index.ts
@@ -0,0 +1,17 @@
+require("dotenv").config();
+
+import express from "express";
+import morgan from "morgan";
+
+const port = +(process.env.PORT || 3333);
+const app = express();
+
+app.use(morgan("dev"));
+
+app.get("/", (request, response) => {
+  return response.status(200).send("Hello, world!");
+});
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`);
+});
